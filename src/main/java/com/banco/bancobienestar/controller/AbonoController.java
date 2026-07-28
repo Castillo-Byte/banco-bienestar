f86b772ef2095b36633ff4194f9c6588893989b5
Carlos Eduardo Eugenio Castillo <cecastillo178dj@gmail.com>
2026-07-28 11:23:30 -0600

    corregi el dockerfile me faltaba un espacio en el entrypoint

---
+++ src/main/java/com/banco/bancobienestar/controller/AbonoController.java
@@ -0,0 +1,15 @@
+package com.banco.bancobienestar.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class AbonoController {
+
+    @GetMapping("/abono")
+    public String mostrarAbono() {
+        return "abono";
+    }
+    
+}
